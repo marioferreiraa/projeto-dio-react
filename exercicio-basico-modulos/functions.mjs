c81b3b8972a35bb61cb1e5a4fab981a5166bfac9
Mario Ferreira <acc01365@sky.com.br>
2021-10-18 07:46:05 -0300

    Exercicio basico de modulos

---
+++ exercicio-basico-modulos/functions.mjs
@@ -0,0 +1,17 @@
+function mostrarIdade(nome, idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostrarCidade(nome, cidade){
+    return `A cidade de ${nome} é ${cidade}`
+}
+
+function mostrarHobbie(nome, hobbie){
+    return `O hobbie de ${nome} é ${hobbie}`
+}
+
+export {
+    mostrarCidade,
+    mostrarIdade,
+    mostrarHobbie
+}
---
+++ exercicio-basico-modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostrarIdade, mostrarCidade, mostrarHobbie } from "./functions.mjs";
+
+console.log(mostrarIdade("Mário", 24));
+console.log(mostrarCidade("Mário", "Recife"));
+console.log(mostrarHobbie("Mário", "Programar"));
